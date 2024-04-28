284d600804aaaf754de6a11ab1a29369c3e00451
Luzin7 <lvictor.dutra@gmail.com>
2024-04-28 15:42:49 -0300

    remove

--- src/modules/movies/useCases/deleteMovie/deleteMovie.useCase.dto.ts
+++
@@ -1,8 +0,0 @@
-import { MovieNotFoundError } from '@modules/movies/errors/movieNotFound.error';
-import { Either } from '@shared/core/errors/Either';
-
-export type InputDeleteMovieDto = {
-  id: string;
-};
-
-export type OutputDeleteMovieDto = Either<MovieNotFoundError, null>;
--- src/modules/movies/useCases/findAllMovies/findAll.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Either } from '@shared/core/errors/Either';
-import { OutputMovieDto } from '../../repositories/contracts/movie.dto';
-
-export type OutputFindAllMoviesDto = Either<
-  null,
-  {
-    movies: OutputMovieDto[];
-  }
->;
