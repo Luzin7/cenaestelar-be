4e963f0b5396f7f8092a9e8b3f54f1798966f1a6
Luzin7 <lvictor.dutra@gmail.com>
2024-05-05 18:25:18 -0300

    feat: add update movie route

--- src/infra/http/routes/movie/index.ts
+++ src/infra/http/routes/movie/index.ts
@@ -1,12 +1,14 @@
+import { CreateMovie } from '@infra/http/controllers/movie/createMovie/createMovie.controller';
 import { DeleteMovieById } from '@infra/http/controllers/movie/deleteMovie/deleteMovie.controller';
+import { FindAllMovies } from '@infra/http/controllers/movie/findAllMovies/findAll.useCase';
+import { FindMovieById } from '@infra/http/controllers/movie/findMovieById/findMovieById.useCase';
+import { FindMovieByTitle } from '@infra/http/controllers/movie/findMovieByTitle/findMovieByTitle.useCase';
 import { FindMoviesByRating } from '@infra/http/controllers/movie/findMoviesByRating/findMovieByTitle.useCase';
+import { UpdateMovie } from '@infra/http/controllers/movie/updateMovie/updateMovie.controller';
 import { Request, Response, Router } from 'express';
-import { CreateMovie } from '../../controllers/movie/createMovie/createMovie.controller';
-import { FindAllMovies } from '../../controllers/movie/findAllMovies/findAll.useCase';
-import { FindMovieById } from '../../controllers/movie/findMovieById/findMovieById.useCase';
-import { FindMovieByTitle } from '../../controllers/movie/findMovieByTitle/findMovieByTitle.useCase';
 
 const createMovieController = new CreateMovie();
+const updateMovieController = new UpdateMovie();
 const findAllMoviesController = new FindAllMovies();
 const findMovieByIdController = new FindMovieById();
 const findMovieByTitleController = new FindMovieByTitle();
@@ -26,5 +28,6 @@ movieRoutes.get('/movies', (req: Request, res: Response) => {
 
 movieRoutes.get('/movie/:id', findMovieByIdController.handle);
 movieRoutes.post('/movies', createMovieController.handle);
+movieRoutes.put('/movie/:id', updateMovieController.handle);
 movieRoutes.delete('/movie/:id', deleteMovieByIdController.handle);
 movieRoutes.get('/movies/rating', findMovieByRatingController.handle);
