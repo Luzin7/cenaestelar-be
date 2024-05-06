6a3b8ae393090f4f61918c0a92a430e560bd8891
Luzin7 <lvictor.dutra@gmail.com>
2024-05-06 20:38:13 -0300

    feat: add top movies route

--- src/infra/http/routes/movie/index.ts
+++ src/infra/http/routes/movie/index.ts
@@ -1,6 +1,7 @@
 import { CreateMovie } from '@infra/http/controllers/movie/createMovie/createMovie.controller';
 import { DeleteMovieById } from '@infra/http/controllers/movie/deleteMovie/deleteMovie.controller';
 import { FindAllMovies } from '@infra/http/controllers/movie/findAllMovies/findAll.useCase';
+import { FindBestMoviesSeen } from '@infra/http/controllers/movie/findBestMoviesSeen/findBestMoviesSeen.controller';
 import { FindMovieById } from '@infra/http/controllers/movie/findMovieById/findMovieById.useCase';
 import { FindMovieByTitle } from '@infra/http/controllers/movie/findMovieByTitle/findMovieByTitle.useCase';
 import { FindMoviesByRating } from '@infra/http/controllers/movie/findMoviesByRating/findMovieByTitle.useCase';
@@ -10,6 +11,7 @@ import { Request, Response, Router } from 'express';
 const createMovieController = new CreateMovie();
 const updateMovieController = new UpdateMovie();
 const findAllMoviesController = new FindAllMovies();
+const findBestMoviesSeen = new FindBestMoviesSeen();
 const findMovieByIdController = new FindMovieById();
 const findMovieByTitleController = new FindMovieByTitle();
 const deleteMovieByIdController = new DeleteMovieById();
@@ -27,6 +29,7 @@ movieRoutes.get('/movies', (req: Request, res: Response) => {
 });
 
 movieRoutes.get('/movie/:id', findMovieByIdController.handle);
+movieRoutes.get('/movies/top', findBestMoviesSeen.handle);
 movieRoutes.post('/movies', createMovieController.handle);
 movieRoutes.put('/movie/:id', updateMovieController.handle);
 movieRoutes.delete('/movie/:id', deleteMovieByIdController.handle);
