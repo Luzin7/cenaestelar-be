1a46d0597fde1e771c721f4b9a38c24cdbe2017e
Luzin7 <lvictor.dutra@gmail.com>
2024-05-06 20:38:40 -0300

    feat: add top movies implementations

--- src/infra/implementations/movies/index.ts
+++ src/infra/implementations/movies/index.ts
@@ -2,12 +2,16 @@ import { MoviesRepositoryImplementations } from '@infra/database/movie/Movie.rep
 import { CreateMovie } from '@modules/movies/useCases/createMovie/createMovie.useCase';
 import { DeleteMovie } from '@modules/movies/useCases/deleteMovie/deleteMovie.useCase';
 import { FindAllMovies } from '@modules/movies/useCases/findAllMovies/findAll.useCase';
+import { FindBestMoviesSeen } from '@modules/movies/useCases/findBestMoviesSeen/findBestMoviesSeen.useCase';
 import { FindMovieById } from '@modules/movies/useCases/findMovieById/findMovieById.useCase';
 import { FindMovieByTitle } from '@modules/movies/useCases/findMovieByTitle/findMovieByTitle.useCase';
 import { FindMoviesByRating } from '@modules/movies/useCases/findMoviesByRating/findMoviesByRating.useCase';
 import { UpdateMovie } from '@modules/movies/useCases/updateMovie/updateMovie.useCase';
 
 export const createMovieUseCase = new CreateMovie(
+  new MoviesRepositoryImplementations(),
+);
+export const findBestMoviesSeenUseCase = new FindBestMoviesSeen(
   new MoviesRepositoryImplementations(),
 );
 export const updateMovieUseCase = new UpdateMovie(
